98f8b3efcf091ec98ccb04e92c2d3d92df36288a
Pranav Kumar <15886100+pkanshu@users.noreply.github.com>
2023-07-18 10:44:48 +0530

    Create inbox.ts

---
+++ inbox.ts
@@ -0,0 +1,15 @@
+function evaluateProductRequirements() {
+}
+
+function evaluateTechnicalDesign() {
+}
+
+function buildQnASystem() {
+  
+}
+
+// speech, recognition system
+
+// go to office
+
+// build habit
